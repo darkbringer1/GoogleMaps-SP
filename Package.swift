4974ceda3cdd6ab58bb354dffec0f1ef48668eee
Doğukaan Kılıçarslan <dogukaan.k@hotmail.com>
2024-02-26 12:36:52 +0300

    Localize frameworks
    # Conflicts:
    #	Package.swift

--- Package.swift
+++ Package.swift
@@ -41,28 +41,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GoogleMaps",
-            url: "https://github.com/opendoor-labs/GoogleMaps-SP/releases/download/5.1.0-M1/GoogleMaps.xcframework.zip",
-            checksum: "2f5f3e341abaa31e410bffe8481f303394af28d51dc166b05fd3cdd33b0d3e38"
+            path: "Build/GoogleMaps.xcframework"
         ),
         .binaryTarget(
             name: "GoogleMapsBase",
-            url: "https://github.com/opendoor-labs/GoogleMaps-SP/releases/download/5.1.0-M1/GoogleMapsBase.xcframework.zip",
-            checksum: "4a5423fbf27719115924066195fc719b8f01db635a223d23ddae0d73ef7f97ea"
+            path: "Build/GoogleMapsBase.xcframework"
         ),
         .binaryTarget(
             name: "GoogleMapsCore",
-            url: "https://github.com/opendoor-labs/GoogleMaps-SP/releases/download/5.1.0-M1/GoogleMapsCore.xcframework.zip",
-            checksum: "31795938133da2979a763e4acc01b232ad2383be1eaeac1fc681277dd81324ff"
+            path: "Build/GoogleMapsCore.xcframework"
         ),
         .binaryTarget(
             name: "GoogleMapsM4B",
-            url: "https://github.com/opendoor-labs/GoogleMaps-SP/releases/download/5.1.0-M1/GoogleMapsM4B.xcframework.zip",
-            checksum: "f382729be13d6eb148dd85db64e0c6b23c8236fc103017a32f1fd305ff5a7022"
+            path: "Build/GoogleMapsM4B.xcframework"
         ),
         .binaryTarget(
             name: "GooglePlaces",
-            url: "https://github.com/opendoor-labs/GoogleMaps-SP/releases/download/5.1.0-M1/GooglePlaces.xcframework.zip",
-            checksum: "8ccb07569427a8cf303fb69e486ac2cdef29c5af8af5717a1b3520f9f0210d49"
+            path: "Build/GooglePlaces.xcframework"
         )
     ]
 )
